c5e27269674e5305f53e0dfd6dae2df044ed554a
DeisDev <aidenanthonydeis@gmail.com>
2025-05-28 01:04:46 -0600

    Fix syntax error in version display line in appinstaller.ps1

--- appinstaller.ps1
+++ appinstaller.ps1
@@ -1,6 +1,6 @@
 Write-Host "=============================================" -ForegroundColor DarkGray
 Write-Host "AppInstaller by DeisDev" -ForegroundColor Cyan
-Write-Host "v0.2.7a -ForegroundColor DarkGray
+Write-Host "v0.2.7a" -ForegroundColor DarkGray
 Write-Host "Apache License 2.0 - https://www.apache.org/licenses/LICENSE-2.0" -ForegroundColor DarkGray
 Write-Host "=============================================" -ForegroundColor DarkGray
 
